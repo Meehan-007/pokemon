82f625f7fec5fb682243a8be11dd45dc7c89c478
Patrick Meehan <patrickmeehan@Patricks-MacBook-Air.local>
2025-07-31 10:46:41 -0400

    changing loading state

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setcurrentPageUrl] = useState(['https://pokeapi.co/api/v2/pokemon'])
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+    axios.get(currentPageUrl).then(res => { 
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
   }, [])
